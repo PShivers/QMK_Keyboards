6f36ce78d6ef95b2e380497aad46daf24337bede
PShivers <Paul.S.Shivers@gmail.com>
2023-03-31 12:21:24 -0400

    made the comment key work for vs code instead of visual studio

--- planck/keymaps/colemak-dh_planck_v5_2.c
+++ planck/keymaps/colemak-dh_planck_v5_2.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_FKEYSNUMPAD] = LAYOUT(KC_ESC, KC_F9, KC_F10, KC_F11, KC_F12, SELECTALL, KC_NO, KC_7, KC_8, KC_9, KC_PPLS, KC_DEL,
         KC_BSPC, KC_F5, KC_F6, KC_F7, KC_F8, UNCOMMENT, KC_NO, KC_4, KC_5, KC_6, KC_PMNS, KC_NO,
-        KC_LSFT, KC_F1, KC_F2, KC_F3, KC_F4, COMMENT, KC_NO, KC_1, KC_2, KC_3, KC_PAST, KC_NO,
+        KC_LSFT, KC_F1, KC_F2, KC_F3, KC_F4, LCTL(KC_SLSH), KC_NO, KC_1, KC_2, KC_3, KC_PAST, KC_NO,
         KC_NO, KC_NO, LCTL(LGUI(KC_LEFT)), LCTL(LGUI(KC_RGHT)), KC_TRNS, KC_NO, KC_NO, KC_0, KC_PEQL, KC_PDOT, KC_PSLS, KC_NO
     ),
 
@@ -60,6 +60,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
         }
         break;
     case COMMENT:
+        //this is the way to comment in Visual Studio 
         if (record->event.pressed) {
             SEND_STRING(SS_LCTL("kc"))
         }
@@ -67,6 +68,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
 
         }
     case UNCOMMENT:
+        //this is the way to uncomment in Visual Studio
         if (record->event.pressed) {
             SEND_STRING(SS_LCTL("ku"))
         }
